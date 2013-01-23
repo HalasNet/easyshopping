aa27181f5d86885026db6808f7c12f6db5ff133b
qiuzhiquan <qzhiquan@cn.ibm.com>
2013-01-23 17:03:28 +0800

    add spring security module

---
+++ src/main/java/com/ibm/dao/AuthorityDao.java
@@ -0,0 +1,13 @@
+package com.ibm.dao;
+
+import com.ibm.domain.Authority;
+
+/**
+ * 
+ * @author qzq
+ * 
+ * @time 2013-1-23 下午4:48:52
+ */
+public interface AuthorityDao extends IBaseDao<Authority,Long>{
+
+}
