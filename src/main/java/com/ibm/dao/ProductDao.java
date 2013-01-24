f58e89616f6d3d547f0c0b790ac426383f0a45be
daigenshan <523641320@qq.com>
2013-01-24 15:47:24 +0800

    提交实现类productDao

--- src/main/java/com/ibm/dao/ProductDao.java
+++ src/main/java/com/ibm/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.ibm.dao;
 
+import java.util.List;
+
 import com.ibm.domain.Product;
 
 public interface ProductDao extends IBaseDao<Product, Long> 
 {
 
+	public List<Product> queryProductList(Long categoryId, String productName);
 }
--- src/main/java/com/ibm/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/ibm/dao/impl/ProductDaoImpl.java
@@ -1,5 +1,8 @@
 package com.ibm.dao.impl;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ibm.dao.BaseHibernateDao;
@@ -9,6 +12,18 @@ import com.ibm.domain.Product;
 @Repository("productDao")
 public class ProductDaoImpl extends BaseHibernateDao<Product, Long> implements ProductDao 
 {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Product> queryProductList(Long categoryId, String productName) {
+		Query query = this.getSession().createQuery(
+				"from Product where categoryId=? or productname=?");
+		query.setLong(0, categoryId);
+		query.setString(1, productName);
+		List<Product> products = query.list();
+		return products;
+
+	}
 
 
 }
