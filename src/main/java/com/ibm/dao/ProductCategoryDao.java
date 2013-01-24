763207ad9393b409720ed87c579613ab00de6c54
daigenshan <523641320@qq.com>
2013-01-24 15:07:46 +0800

    提交dao

---
+++ src/main/java/com/ibm/dao/ProductCategoryDao.java
@@ -0,0 +1,14 @@
+package com.ibm.dao;
+
+import java.util.List;
+
+import com.ibm.domain.ProductCategory;
+
+public interface ProductCategoryDao extends IBaseDao<ProductCategory, Long> 
+{
+	//根据类别名称查询产品类别，再获取类别ID
+	public ProductCategory query(String categoryName);
+	
+	//查询所有产品类别
+	public List<ProductCategory> query();
+}
---
+++ src/main/java/com/ibm/dao/ProductDao.java
@@ -0,0 +1,8 @@
+package com.ibm.dao;
+
+import com.ibm.domain.Product;
+
+public interface ProductDao extends IBaseDao<Product, Long> 
+{
+
+}
