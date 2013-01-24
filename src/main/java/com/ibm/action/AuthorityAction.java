ac1843e95d9f6bbc9d800774ded93b539e52b4ae
yangxueming <hotyxm@163.com>
2013-01-24 17:43:10 +0800

    yxm

---
+++ src/main/java/com/ibm/action/AuthorityAction.java
@@ -0,0 +1,135 @@
+package com.ibm.action;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+
+import com.ibm.domain.Authority;
+import com.ibm.service.user.AuthorityService;
+import com.opensymphony.xwork2.ActionSupport;
+
+@ParentPackage("publicPackage")
+@Namespace("/admin")
+@SuppressWarnings("serial")
+@Results( { @Result(name = "authview", location = "/add_authority.jsp"),
+		@Result(name = "authmodify", location = "/mod_authority.jsp"),
+		@Result(name = "success", location = "/authority.jsp"),
+		@Result(name = "error", location = "/error.jsp") })
+public class AuthorityAction extends ActionSupport {
+
+	private Long id;
+
+	private Authority authority;
+
+	private AuthorityService authorityService;
+
+	private List<Authority> list;
+
+	public String index() {
+		list = authorityService.queryAll();
+		if (list != null) {
+			return "success";
+		} else {
+			return "error";
+		}
+	}
+	
+	public String view(){
+		String name = authority.getName();
+		if(StringUtils.isBlank(name)){
+			return "success";
+		}
+		list = authorityService.getAuthorityByName(name);
+		if (list != null) {
+			return "success";
+		} else {
+			return "error";
+		}
+	}
+
+	/**
+	 * 进入新建页面
+	 * 
+	 * @return
+	 */
+	public String viewCreate() {
+		return "authview";
+	}
+
+	/**
+	 * 进入修改页面
+	 * 
+	 * @return
+	 */
+	public String viewModify() {
+		authority = authorityService.getAuthority(id);
+		return "authmodify";
+	}
+
+	/**
+	 * 保存
+	 * 
+	 * @return
+	 */
+	public String save() {
+		authorityService.save(authority);
+		return index();
+	}
+
+	/**
+	 * 修改
+	 * 
+	 * @return
+	 */
+	public String modify() {
+		authorityService.modify(authority);
+		return index();
+	}
+
+	/**
+	 * 删除
+	 * 
+	 * @return
+	 */
+	public String del() {
+		authorityService.del(id);
+		return index();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Authority getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(Authority authority) {
+		this.authority = authority;
+	}
+
+	public AuthorityService getAuthorityService() {
+		return authorityService;
+	}
+
+	public void setAuthorityService(AuthorityService authorityService) {
+		this.authorityService = authorityService;
+	}
+
+	public List<Authority> getList() {
+		return list;
+	}
+
+	public void setList(List<Authority> list) {
+		this.list = list;
+	}
+
+}
--- src/main/java/com/ibm/service/user/AuthorityService.java
+++ src/main/java/com/ibm/service/user/AuthorityService.java
@@ -1,15 +1,21 @@
 package com.ibm.service.user;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ibm.dao.AuthorityDao;
+import com.ibm.dao.LogDao;
+import com.ibm.dao.UserDao;
 import com.ibm.domain.Authority;
+import com.ibm.domain.Log;
+import com.ibm.domain.User;
 
 /**
  * 
@@ -23,9 +29,48 @@ public class AuthorityService {
 
 	@Resource
 	private AuthorityDao authorityDao;
-	
-	@Transactional(propagation=Propagation.NOT_SUPPORTED,readOnly=true)
-	public List<Authority> queryAll(){
+
+	@Autowired
+	private LogDao logDao;
+
+	@Transactional(propagation = Propagation.NOT_SUPPORTED, readOnly = true)
+	public List<Authority> queryAll() {
 		return authorityDao.listAll();
+	}
+	
+	@Transactional
+	public List<Authority> getAuthorityByName(String name) {
+		return authorityDao.getAuthorityByName(name);
+	}
+	
+
+	@Transactional
+	public void save(Authority authority) {
+		authorityDao.save(authority);
+		Log log = new Log();
+		log.setRemark("add authority:" + authority.getName());
+		log.setLogTime(new Date());
+		logDao.save(log);
+	}
+	
+	@Transactional
+	public void modify(Authority authority) {
+		authorityDao.update(authority);
+		Log log = new Log();
+		log.setRemark("modify authority:" + authority.getName());
+		log.setLogTime(new Date());
+		logDao.save(log);
+	}
+
+	@Transactional
+	public Authority getAuthority(Long id) {
+		return authorityDao.get(id);
+
+	}
+	
+	@Transactional
+	public void del(Long id) {
+		authorityDao.delete(id);
+
 	}
 }
