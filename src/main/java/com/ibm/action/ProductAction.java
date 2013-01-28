7199ad1ba22fc492fb9044c60e6bb1317a66d9b4
lianzhengkun <lianzhengkun@gmail.com>
2013-01-28 16:43:37 +0800

    page demo

--- src/main/java/com/ibm/action/ProductAction.java
+++ src/main/java/com/ibm/action/ProductAction.java
@@ -1,7 +1,9 @@
 package com.ibm.action;
 
 import java.util.List;
+import java.util.Map;
 
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
@@ -16,6 +18,7 @@ import com.ibm.domain.ProductCategory;
 import com.ibm.service.product.ProductService;
 import com.ibm.service.user.ProductCategoryService;
 import com.ibm.util.Constants;
+import com.ibm.util.page.Pagination;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 
@@ -41,7 +44,14 @@ public class ProductAction extends ActionSupport implements ModelDriven<Object> 
 	private List<ProductCategory> categoryList;
 	private long categoryId;
 	private String productName;
-	private int pn;
+	private Pagination pagination;
+	private int total;
+	private int curPage;
+	private int prePage;
+	private int nextPage;
+	private int totalPage;
+	private int startNum;
+	private int endNum;
 
 	// 获取 id 请求参数的方法
 	public void setId(long id) {
@@ -58,8 +68,18 @@ public class ProductAction extends ActionSupport implements ModelDriven<Object> 
 
 	// 处理不带 id 参数的 GET 请求
 	public HttpHeaders index() {
-		list = productService.getAll();
+		pagination = new Pagination(curPage, Constants.DEFAULT_PAGE_SIZE);
+		list = productService.search(categoryId, model.getProductName(),
+				pagination);
+		pagination = pagination.getPage();
 		categoryList = productCategoryService.queryCategorys();
+		total = pagination.getTotal();
+		curPage = pagination.getCurPage();
+		prePage = pagination.getPrePage();
+		nextPage = pagination.getNextPage();
+		totalPage = pagination.getTotalPage();
+		startNum = pagination.getStartNum();
+		endNum = pagination.getEndNum();
 		return new DefaultHttpHeaders("index").disableCaching();
 	}
 
@@ -103,8 +123,10 @@ public class ProductAction extends ActionSupport implements ModelDriven<Object> 
 	}
 
 	public String search() {
-		list = productService.search(categoryId, model.getProductName(), pn,
-				Constants.DEFAULT_PAGE_SIZE);
+		pagination = new Pagination(curPage, Constants.DEFAULT_PAGE_SIZE);
+		list = productService.search(categoryId, model.getProductName(),
+				pagination);
+		pagination = pagination.getPage();
 		categoryList = productCategoryService.queryCategorys();
 		return "success";
 	}
@@ -138,20 +160,76 @@ public class ProductAction extends ActionSupport implements ModelDriven<Object> 
 		return productName;
 	}
 
-	public int getPn() {
-		return pn;
-	}
-
-	public void setPn(int pn) {
-		this.pn = pn;
-	}
-
 	public void setCategoryId(long categoryId) {
 		this.categoryId = categoryId;
 	}
 
 	public void setProductName(String productName) {
 		this.productName = productName;
+	}
+
+	public Pagination getPagination() {
+		return pagination;
+	}
+
+	public void setPagination(Pagination pagination) {
+		this.pagination = pagination;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public int getCurPage() {
+		return curPage;
+	}
+
+	public void setCurPage(int curPage) {
+		this.curPage = curPage;
+	}
+
+	public int getPrePage() {
+		return prePage;
+	}
+
+	public void setPrePage(int prePage) {
+		this.prePage = prePage;
+	}
+
+	public int getNextPage() {
+		return nextPage;
+	}
+
+	public void setNextPage(int nextPage) {
+		this.nextPage = nextPage;
+	}
+
+	public int getTotalPage() {
+		return totalPage;
+	}
+
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+
+	public int getStartNum() {
+		return startNum;
+	}
+
+	public void setStartNum(int startNum) {
+		this.startNum = startNum;
+	}
+
+	public int getEndNum() {
+		return endNum;
+	}
+
+	public void setEndNum(int endNum) {
+		this.endNum = endNum;
 	}
 
 }
--- src/main/java/com/ibm/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/ibm/dao/impl/ProductDaoImpl.java
@@ -12,23 +12,26 @@ import com.ibm.dao.ProductDao;
 import com.ibm.domain.Product;
 import com.ibm.util.ConditionQuery;
 import com.ibm.util.OrderBy;
+import com.ibm.util.page.Pagination;
 
 @Repository("productDao")
 public class ProductDaoImpl extends BaseHibernateDao<Product, Long> implements ProductDao 
 {
 
 	@Override
-	public List<Product> queryProductList(Long categoryId, String productName,int pn,int pageSize) {
+	public List<Product> queryProductList(Long categoryId, String productName,Pagination pagination) {
 		ConditionQuery query=new ConditionQuery();
 		if(StringUtils.isNotBlank(productName)){
-			query.add(Restrictions.like("productName", productName));
+			query.add(Restrictions.like("productName", "%"+productName+"%"));
 		}
 		if(categoryId!=null && categoryId.longValue()!=0){
 			query.add(Restrictions.eq("category.id", categoryId));
 		}
 		OrderBy orderBy =new OrderBy();
 		orderBy.add(Order.desc("id"));
-		List<Product> products = this.list(query, orderBy, pn, pageSize);
+		
+		List<Product> products = this.list(query, orderBy, pagination.getCurPage(), pagination.getPageSize());
+		pagination.setTotal(this.countList(query));
 		return products;
 
 	}
