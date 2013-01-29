99fd9d87d6f2a45f83bfdff30d258fc4e3ce7702
daigenshan <523641320@qq.com>
2013-01-29 13:21:16 +0800

    提交批量删除

--- src/main/java/com/ibm/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/ibm/dao/impl/ProductDaoImpl.java
@@ -3,6 +3,7 @@ package com.ibm.dao.impl;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.hibernate.Query;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import com.ibm.dao.BaseHibernateDao;
 import com.ibm.dao.ProductDao;
 import com.ibm.domain.Product;
+import com.ibm.domain.ProductCategory;
 import com.ibm.util.ConditionQuery;
 import com.ibm.util.OrderBy;
 import com.ibm.util.page.Pagination;
@@ -34,6 +36,14 @@ public class ProductDaoImpl extends BaseHibernateDao<Product, Long> implements P
 		pagination.setTotal(this.countList(query));
 		return products;
 
+	}
+
+	@Override
+	public List<Product> queryProductList(String categoryIds) {
+		Query query = this.getSession().createQuery(
+				"from Product where id in ("+categoryIds+")");
+		List<Product> listProducts = query.list();
+		return listProducts;
 	}
 
 
