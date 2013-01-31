e8011a10a785d215bd70bd3eb975d1bfd539c2f4
qiuzhiquan <qzhiquan@cn.ibm.com>
2013-01-31 11:23:24 +0800

    user

--- src/main/java/com/ibm/dao/UserDao.java
+++ src/main/java/com/ibm/dao/UserDao.java
@@ -1,5 +1,8 @@
 package com.ibm.dao;
 
+import java.util.Collection;
+import java.util.List;
+
 import com.ibm.domain.User;
 
 public interface UserDao extends IBaseDao<User, Long>{
@@ -7,4 +10,6 @@ public interface UserDao extends IBaseDao<User, Long>{
 	public User getUserByNameAndPwd(String name,String password);
 
 	public User getUserByName(String username);
+	
+	public List<User> queryUserByIds(Long[] ids);
 }
