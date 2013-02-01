848e107292ae1f81eaed43f14b2a92730bd52405
qiuzhiquan <qzhiquan@cn.ibm.com>
2013-02-01 14:50:24 +0800

    add "/" to authority

--- src/main/resources/system.sql
+++ src/main/resources/system.sql
@@ -18,11 +18,11 @@ CREATE TABLE `t_authority` (
 -- ----------------------------
 -- Records of t_authority
 -- ----------------------------
-INSERT INTO `t_authority` VALUES ('1', 'user_mgr', '用户管理', 'user manage', 'admin/user');
-INSERT INTO `t_authority` VALUES ('2', 'role_mgr', '角色管理相关权限', '角色管理', 'admin/role');
-INSERT INTO `t_authority` VALUES ('3', 'authority_mgr', '权限管理', '权限管理', 'admin/authority');
-INSERT INTO `t_authority` VALUES ('4', 'product_mgr', '商品管理', '商品管理', 'admin/product');
-INSERT INTO `t_authority` VALUES ('5', 'product_kind_mgr', '商品类别管理', '商品类别管理', 'admin/ProductCategory');
+INSERT INTO `t_authority` VALUES ('1', 'user_mgr', '用户管理', 'user manage', '/admin/user');
+INSERT INTO `t_authority` VALUES ('2', 'role_mgr', '角色管理相关权限', '角色管理', '/admin/role');
+INSERT INTO `t_authority` VALUES ('3', 'authority_mgr', '权限管理', '权限管理', '/admin/authority');
+INSERT INTO `t_authority` VALUES ('4', 'product_mgr', '商品管理', '商品管理', '/admin/product');
+INSERT INTO `t_authority` VALUES ('5', 'product_kind_mgr', '商品类别管理', '商品类别管理', '/admin/ProductCategory');
 
 -- ----------------------------
 -- Table structure for `t_role`
