5a582f6bf669609e40ff2867e15fe5728dcb3457
luinstein <luinstein@gmail.com>
2013-02-19 17:32:17 +0800

    product:search

--- src/main/java/com/ibm/action/ProductAction.java
+++ src/main/java/com/ibm/action/ProductAction.java
@@ -90,7 +90,6 @@ public class ProductAction extends ActionSupport implements ModelDriven<Object> 
 	public HttpHeaders addProduct() {
 		initCategory();
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-		System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
 		product.setProductPublishDate(df.format(new Date()));
 		productService.saveOrUpdate(product);
 		return new DefaultHttpHeaders("add_product").disableCaching();
